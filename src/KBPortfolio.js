f00daf66fd90680115ca0918969f07bf7e1f092d
Asia Carter <asiamcarter@gmail.com>
2019-05-31 11:01:28 -0500

    intro page complete save for image

--- src/KBPortfolio.js
+++ src/KBPortfolio.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react'
-import ApplicationViews from "./ApplicationViews"
-
+import React, { Component } from "react";
+import ApplicationViews from "./ApplicationViews";
+import "./KBPortfolio.css";
 
 export default class KBPortfolio extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Student Kennels</h3>
-                <h4>Nashville North Location</h4>
-                <h5>500 Puppy Way</h5>
-                <ApplicationViews />
-            </div>
-        );
+  state = {
+    buttonClicked: false
+  };
+
+  buttonClicked = () => {
+    this.setState({ buttonClicked: true });
+  };
+
+  pageRender = () => {
+    if (this.state.buttonClicked !== true) {
+      return (
+        <div className="intro-image-div">
+          {/* <img
+                src="https://res.cloudinary.com/dxeuwue7t/image/upload/v1559253675/c1b1704e0217b1d5fb2f8735719006a2_hoh0g6.png"
+                alt="line drawing of artist Kyle Baker"
+                className="intro-image"
+              /> */}
+          <div className="intro-text-div">
+            <button className="intro-button" onClick={this.buttonClicked}>
+              <h1 className="intro-text">KYLE BAKER</h1>
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return <ApplicationViews />;
     }
+  };
+
+  render() {
+    return this.pageRender();
+  }
 }
