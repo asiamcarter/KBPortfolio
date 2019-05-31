455ff01202275f84e19fec5dcae0a78bb3a05578
Asia Carter <asiamcarter@gmail.com>
2019-05-31 11:52:40 -0500

    front page complete for now

--- src/KBPortfolio.js
+++ src/KBPortfolio.js
@@ -14,16 +14,18 @@ export default class KBPortfolio extends Component {
   pageRender = () => {
     if (this.state.buttonClicked !== true) {
       return (
-        <div className="intro-image-div">
-          {/* <img
-                src="https://res.cloudinary.com/dxeuwue7t/image/upload/v1559253675/c1b1704e0217b1d5fb2f8735719006a2_hoh0g6.png"
-                alt="line drawing of artist Kyle Baker"
-                className="intro-image"
-              /> */}
-          <div className="intro-text-div">
-            <button className="intro-button" onClick={this.buttonClicked}>
-              <h1 className="intro-text">KYLE BAKER</h1>
-            </button>
+        <div className="intro-background">
+          <div className="intro-image-div">
+            {/* <img
+            src="https://res.cloudinary.com/dxeuwue7t/image/upload/v1559253675/c1b1704e0217b1d5fb2f8735719006a2_hoh0g6.png"
+            alt="line drawing of artist Kyle Baker"
+            className="intro-image"
+          /> */}
+            <div className="intro-text-div">
+              <button className="intro-button" onClick={this.buttonClicked}>
+                <h1 className="intro-text">KYLE BAKER</h1>
+              </button>
+            </div>
           </div>
         </div>
       );
